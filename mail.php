efeb5d88d4de5d85da670b110fb7d10274a3e514
Ikinidae <federicaelia24@gmail.com>
2022-06-27 17:15:49 +0200

    snack 2 completato

--- mail.php
+++ mail.php
@@ -1,50 +1,25 @@
-<?php
-    // creo una variabile con la parola inserita dall'utente nel form
-    // utilizzando isset per non avere errore iniziale
-    if (isset($_GET["name"])) {
+<?php    
+    // creo un unico if in cui utilizzo isset per non avere errori iniziali
+    if (isset($_GET["name"]) && isset($_GET["mail"]) && isset($_GET["age"])) {
+        // assegno le variabili per i 3 dati
         $name = $_GET["name"];
-        // controllo che il nome sia più lungo di 3 caratteri, se sì stampo "OK"
-        // funziona
-        if (strlen($name)>3) {
-            $message = 'OK';
-        } else {
-            $message = 'NO';
-        };
-        echo $message;
-    };
-
-    if (isset($_GET["mail"])) {
         $mail = $_GET["mail"];
-        $position_at_sign = strpos($mail, '@');
-        if ($position_at_sign !== false) {
-            $position_dot = strpos($mail, '.', $position_at_sign);
-            if ($position_dot !== false) {
-                $message = 'OK';
-            } else {
-                $message = 'NO';
-            }
-        } else {
-            $message = 'NO';
-        };
-        echo $message;
-    };
-
-    if (isset($_GET["age"])) {
         $age = $_GET["age"];
-        // controllo che il nome sia più lungo di 3 caratteri, se sì stampo "OK"
-        // funziona
-        if (is_numeric($age)) {
-            $message = 'OK';
+
+        // assegno ad una variabile la posizione della chiocchiola
+        $position_at_sign = strpos($mail, '@');
+
+        // assegno ad una variabile la posizione del punto (dopo la chiocchiola)
+        $position_dot = strpos($mail, '.', $position_at_sign);
+
+        if (strlen($name)>3 && is_numeric($age) == true && $position_at_sign !== false && $position_dot !== false) {
+            $message = 'Accesso riuscito';
         } else {
-            $message = 'NO';
-        };
-        echo $message;
-    };
-    
-    // controllo che il nome sia più lungo di 3 caratteri, se sì stampo "OK"
-    // funziona
-    
-    
+            $message = 'Accesso negato';
+        }
+    } else {
+        $message = 'Inserire dati';
+    }
 ?>
 
 <!DOCTYPE html>
@@ -60,28 +35,25 @@
 <body>
     <!-- form per il nome -->
     <form action="" method="get">
-        <label for="name">Inserire nome</label>
-        <input type="text" name="name">
+        <div>
+            <label for="name">Inserire nome</label>
+            <input type="text" name="name">
+        </div>
+
+        <div>
+            <label for="mail">Inserire mail</label>
+            <input type="text" name="mail">
+        </div>
+        
+        <div>
+            <label for="age">Inserire età</label>
+            <input type="text" name="age">
+        </div>
+
         <button>Invia</button>
     </form>
 
-    <!-- form per la mail -->
-    <form action="" method="get">
-        <label for="mail">Inserire mail</label>
-        <input type="text" name="mail">
-        <button>Invia</button>
-    </form>
-
-    <!-- form per l'età -->
-    <form action="" method="get">
-        <label for="age">Inserire età</label>
-        <input type="text" name="age">
-        <button>Invia</button>
-    </form>
-
-    <!-- <h3>Il tuo nome è: <?= $name ?></h3>
-    <h3>La tua mail è: <?= $mail ?></h3>
-    <h3>La tua età è: <?= $age ?></h3> -->
+    <p> <?= $message ?> </p>
 </body>
 
 </html>
